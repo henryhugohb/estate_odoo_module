d562952b8d64ef91ba32e8601dadca457eeea551
Henry <henry.tumbacom@gmail.com>
2025-08-05 09:34:25 -0500

    Add file manifest with base info

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Estate Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Henry",
+    'category': 'Examples',
+    'description': "Example module",
+    # data files always loaded at installation
+    #'data': [
+        #'views/mymodule_view.xml',
+    #],
+    # data files containing optionally loaded demonstration data
+    #'demo': [
+        #'demo/demo_data.xml',
+    #],
+}
